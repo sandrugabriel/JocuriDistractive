bc87e20f3f97b94878f82bfa36db63afedef405b
Sandru Gabriel <gabi200611@yahoo.com>
2023-05-13 22:40:26 +0300

    done

---
+++ JocuriDistractive/Models/Utilizator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JocuriDistractive.Models
+{
+    internal class Utilizator
+    {
+
+        private string email;
+        private string nume;
+        private string password;
+
+        public Utilizator(string email, string nume, string password)
+        {
+            this.email = email;
+            this.nume = nume;
+            this.password = password;
+        }
+
+        public Utilizator(string textul)
+        {
+
+            string[] prop = textul.Split(';');
+
+            this.email = prop[0];
+            this.nume = prop[1];
+            this.password = prop[2];
+
+        }
+
+        public string getemail()
+        {
+            return this.email;
+        }
+
+        public string getnume()
+        {
+            return this.nume;
+        }
+
+        public string getpadd()
+        {
+            return this.password;
+        }
+
+    }
+}
