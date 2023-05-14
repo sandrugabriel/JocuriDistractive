515cdcced50a89c5b0246239bf96dfc07c17f4fc
Sandru Gabriel <gabi200611@yahoo.com>
2023-05-14 09:14:17 +0300

    done

--- JocuriDistractive/Form1.cs
+++ JocuriDistractive/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JocuriDistractive.Panel_uri;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,9 @@ namespace JocuriDistractive
         public Form1()
         {
             InitializeComponent();
+
+            this.Controls.Add(new pnlLogare(this));
+
         }
     }
 }
--- JocuriDistractive/Program.cs
+++ JocuriDistractive/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JocuriDistractive.Mockups;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
